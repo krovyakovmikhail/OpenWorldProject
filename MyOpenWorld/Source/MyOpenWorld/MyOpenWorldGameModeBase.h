083958d9255436cb906f33956221c5b2b5f06031
Misha <m.krovyakov@bk.ru>
2022-06-17 09:48:09 +0300

    Preparing for OW

---
+++ MyOpenWorld/Source/MyOpenWorld/MyOpenWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyOpenWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYOPENWORLD_API AMyOpenWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
